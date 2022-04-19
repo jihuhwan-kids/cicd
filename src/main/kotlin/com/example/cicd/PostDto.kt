1d3cface74811413f1e76b15fea0d3d4d78ba696
jihuhwan-kids <intern220495@github.com>
2022-04-19 19:48:57 +0900

    get,post 적용해보기

---
+++ src/main/kotlin/com/example/cicd/PostDto.kt
@@ -0,0 +1,12 @@
+package com.example.cicd
+
+import lombok.Getter
+import lombok.NoArgsConstructor
+
+@NoArgsConstructor
+@Getter
+class PostDto {
+    val id: Long? = null
+
+    val name: String? = null
+}
