6392cb2bc7367d8f319c7ad59a896019d3369bee
황지훈(LGU/인턴) <103920798+jihuhwan-kids@users.noreply.github.com>
2022-04-22 14:25:07 +0900

    Feature/test (#1)
    * cicd 테스트
    
    * cicd 테스트
    
    * cicd 테스트
    
    * cicd 테스트
    
    * cicd 테스트
    
    * cicd 테스트
    
    * cicd 테스트
    
    * cicd 테스트
    
    * cicd 테스트
    
    * cicd 테스트
    
    * cicd 테스트
    
    * cicd 테스트
    
    * cicd 테스트
    
    * cicd 테스트
    
    * cicd 테스트
    
    * cicd 테스트
    
    * cicd 테스트
    
    * cicd 테스트
    
    * cicd 테스트
    
    * cicd 테스트
    
    * cicd 테스트
    
    * cicd 테스트
    
    * cicd 테스트
    
    * cicd 테스트
    
    Co-authored-by: jihuhwan-kids <intern220495@github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,17 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.6.6"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+//	id ("org.jetbrains.kotlin.plugin.jpa") version "1.3.61"
+	id ("org.jetbrains.kotlin.plugin.allopen") version "1.3.61"
 	kotlin("jvm") version "1.6.10"
 	kotlin("plugin.spring") version "1.6.10"
 }
+
+//allOpen {
+//	annotation("javax.persistence.Entity")
+//	annotation("javax.persistence.MappedSuperclass")
+//	annotation("javax.persistence.Embeddable")
+//}
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
@@ -20,7 +28,6 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.projectlombok:lombok:1.18.24")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
