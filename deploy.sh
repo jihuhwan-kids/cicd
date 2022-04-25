a5938761dd162f7f2898e3bddf985651f96b74d8
jihuhwan-kids <intern220495@github.com>
2022-04-25 19:32:00 +0900

    shell update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 export MYSQL_YUBBI_URL="jdbc:mysql://yubbi-database.csebcqevndde.ap-northeast-2.rds.amazonaws.com:3306/yubbi"
 export MYSQL_YUBBI_ID="admin"
@@ -21,5 +21,5 @@ else
   sleep 5
 fi
 
-DEPLOY_JAR=/home/ec2-user/cicd-0.0.1-SNAPSHOT.jar
+DEPLOY_JAR=/home/ec2-user/*.jar
 nohup java -jar $DEPLOY_JAR > nohup.out 2>&1 &
