b98806e180cf231e9cc79b76e37de69ef65d36fd
jihuhwan-kids <intern220495@github.com>
2022-04-25 14:43:13 +0900

    cicd 테스트

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,17 +3,17 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.6.6"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-//	id ("org.jetbrains.kotlin.plugin.jpa") version "1.3.61"
+	id ("org.jetbrains.kotlin.plugin.jpa") version "1.3.61"
 	id ("org.jetbrains.kotlin.plugin.allopen") version "1.3.61"
 	kotlin("jvm") version "1.6.10"
 	kotlin("plugin.spring") version "1.6.10"
 }
 
-//allOpen {
-//	annotation("javax.persistence.Entity")
-//	annotation("javax.persistence.MappedSuperclass")
-//	annotation("javax.persistence.Embeddable")
-//}
+allOpen {
+	annotation("javax.persistence.Entity")
+	annotation("javax.persistence.MappedSuperclass")
+	annotation("javax.persistence.Embeddable")
+}
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
@@ -29,6 +29,9 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("mysql:mysql-connector-java")
+
 }
 
 tasks.withType<KotlinCompile> {
